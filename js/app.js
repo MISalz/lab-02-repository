6e8dc48f10cc8a3e39a6cd813f6b9c52a6d5da8c
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-04 18:06:50 -0700

    working on making images being built/ appearing on page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log('frankenstein');
+let contentArr = [];
+//// we could put them into an array from the begining
+let testArr = $.ajax('/data/page-1.json');
+console.log(testArr + 'hey');
+
+let Pictures = (image_url, title, description, keyword, horns) => {
+  this.image_url = image_url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+
+  contentArr.push(this);
+
+};
+
+let $imgContainer = testArr
+let $imgOnPage = $('.temp')
+
+$.ajax('/data/page-1.json').then(data => {
+////gets array of images
+  console.log(data);
+
+  data.forEach((pic) => {
+    ////each image
+    console.log(pic);
+    let $createImg = $imgOnPage.clone();
+
+    $createImg.find('img').attr('src', data.image_url);
+    $imgContainer.append($createImg);
+  });
+
+});
+
+
+
