958f4079397d5f978402f9cf21211348fc5ac50d
Michelle Salazar <michellesalazar010@gmail.com>
2020-06-04 22:19:48 -0700

    Render Image

--- js/app.js
+++ js/app.js
@@ -1,38 +1,56 @@
 'use strict';
 
-let contentArr = [];
-//// we could put them into an array from the begining
-let testArr = $.ajax('/data/page-1.json');
-console.log(testArr + 'hey');
+// let contentArr = [];
+// //// we could put them into an array from the begining
+// let testArr = $.ajax('/data/page-1.json');
+// console.log(testArr + 'hey');
 
-let Pictures = (image_url, title, description, keyword, horns) => {
-  this.image_url = image_url;
-  this.title = title;
-  this.description = description;
-  this.keyword = keyword;
-  this.horns = horns;
+// let Pictures = (image_url, title, description, keyword, horns) => {
+//   this.image_url = image_url;
+//   this.title = title;
+//   this.description = description;
+//   this.keyword = keyword;
+//   this.horns = horns;
 
-  contentArr.push(this);
+//   contentArr.push(this);
+// };
 
-};
+// let $imgContainer = $('.img')
+// let $imgOnPage = $('.disp_uni')
+// let $body = $('div')
 
-let $imgContainer = testArr
-let $imgOnPage = $('.temp')
+let uni_list = [];
+let times = 0;
 
 $.ajax('/data/page-1.json').then(data => {
 ////gets array of images
-  console.log(data);
-
-  data.forEach((pic) => {
+  console.log('console1')
+  uni_list.push(data);
+  data.forEach((uni) => {
     ////each image
-    console.log(pic);
-    let $createImg = $imgOnPage.clone();
-
-    $createImg.find('img').attr('src', data.image_url);
-    $imgContainer.append($createImg);
-  });
-
+      console.log('type2');
+      let div = $('.img');
+      div.find('img').attr('src',uni_list[0][0].image_url);
+      //let $$img = $('img');
+      //let img = $$img.clone();
+      //img.attr('src', uni_list.image_url);
+      //console.log('hey bro!');
+      $('.img').append(`<div>${div}</div>`);
+      times++;
+    },
+    console.log('console2'),
+    // let $createImg = $disp_uni.clone();
+    // $createImg.find('.disp_uni').attr('src', uni.image_url);
+    // $imgContainer.append($createImg);
+    );
+    
 });
+
+
+
+
+
+
 
 
 
