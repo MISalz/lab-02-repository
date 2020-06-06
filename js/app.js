639f4879136f539d66d1ee3087292d7d7ec6fc31
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-06 11:35:48 -0700

    have title and description on page but not in the div. still need to get header, img, and p into one div to make a full card instead of parts of a card

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ let img_list = [];
 let i = 0;
 let newArrHorn = [];
 let newArrKey = [];
-let newArr = [];
 
 
 $.ajax('/data/page-1.json').then(data => {
@@ -16,9 +15,14 @@ $.ajax('/data/page-1.json').then(data => {
     let url = img_list[0][i].image_url;
     let hornNum = img_list[0][i].horns;
     let keyword = img_list[0][i].keyword;
-    console.log('hey');
-    imgTemp.find('img').attr('src', url).addClass(keyword).appendTo('main');
-    imgTemp.addClass(hornNum);
+    let title = img_list[0][i].title;
+    let description = img_list[0][i].description;
+
+
+    imgTemp.find('header').text(title).appendTo('.pictures');
+    imgTemp.find('img').attr('src', url).removeClass('imgTemp').addClass(keyword).appendTo('.pictures');
+    imgTemp.find('p').text(description).appendTo('.pictures');
+    imgTemp.appendTo('.pictures');
     newArrHorn.push(hornNum);
     newArrKey.push(keyword);
 
