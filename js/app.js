5de0ef0eaf59c87bd4b878e1c71bd4317c4669f6
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-05 21:51:45 -0700

    all 20 images appear

--- js/app.js
+++ js/app.js
@@ -1,27 +1,22 @@
 'use strict';
 
-let uni_list = [];
+let img_list = [];
 let i = 0;
+// let imgTemp = $('.imgTemp').clone();
 
 $.ajax('/data/page-1.json').then(data => {
 ////gets array of images
-  console.log('console1')
-  uni_list.push(data);
-  data.forEach((uni) => {
-    let div = $('.img');
-    console.log('console2');
-    div.find('img').attr('src', uni_list[0][i].image_url);
-    console.log('console3');
-    // div.appendTo('div');
-    console.log('console3.5');
+  img_list.push(data);
+
+  data.forEach((hornImgs) => {
+    let imgTemp = $('.imgTemp').clone();
+
+    imgTemp.find('img').attr('src', img_list[0][i].image_url).appendTo('main');
+
     i++;
   });
-  console.log('console4');
+
 });
-
-
-
-
 
 
 
