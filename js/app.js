f999b0a1b5aae9b3723301a5ff0f3b041234f5e7
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-06 14:53:37 -0700

    part of lab one changing later

--- js/app.js
+++ js/app.js
@@ -11,27 +11,27 @@ $.ajax('/data/page-1.json').then(data => {
   img_list.push(data);
 
   data.forEach(() => {
-    let imgTemp = $('.imgTemp').clone();
+    let $imgTemp = $('.imgTemp').clone();
     let url = img_list[0][i].image_url;
     let hornNum = img_list[0][i].horns;
     let keyword = img_list[0][i].keyword;
     let title = img_list[0][i].title;
     let description = img_list[0][i].description;
 
+    $imgTemp.find('header').text(title).appendTo('.pictures');
+    $imgTemp.find('img').attr('src', url).removeClass('imgTemp').addClass(keyword).appendTo('.pictures');
+    $imgTemp.find('p').text(description).appendTo('.pictures');
+    // imgTemp.appendTo('.pictures');
 
-    imgTemp.find('header').text(title).appendTo('.pictures');
-    imgTemp.find('img').attr('src', url).removeClass('imgTemp').addClass(keyword).appendTo('.pictures');
-    imgTemp.find('p').text(description).appendTo('.pictures');
-    imgTemp.appendTo('.pictures');
     newArrHorn.push(hornNum);
     newArrKey.push(keyword);
 
     i++;
   });
-
+  console.log('done');
 });
 
-// $('.rhino').
+
 
 
 
