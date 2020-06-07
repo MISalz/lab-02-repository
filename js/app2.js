9b9e510f76b37705549d5f0a139161f8024dce7d
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-06 21:21:26 -0700

    putting together work I worked on in a fork to our main project

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,10 @@
 'use strict';
 // console.log('hey');
+let keywordArr = [];
+let $dropDown = $('.dropDown')
+
+$dropDown.on('change', filterHorns);
+
 
 $(document).ready(function(){
   $.ajax('/data/page-1.json').then(data => {
@@ -10,39 +15,36 @@ $(document).ready(function(){
   })
 });
 
-
 function render(object){
   let $imgTemp = $('#imgTemp').clone();
 
-  $($imgTemp).removeAttr(`id`);
-  //   console.log('bro');
+
   $($imgTemp).addClass(object.keyword);
   $($imgTemp).append(`<header>${object.title}</header>`);
   $($imgTemp).append(`<img src="${object.image_url}"</img>`);
   $($imgTemp).append(`<p>${object.description}</p>`);
   $('#pictures').append($imgTemp);
 
+  if ((keywordArr.includes(object.keyword) === false)){
+    let $dropDown = $('#dropDown');
+
+    keywordArr.push(object.keyword);
+    $($dropDown).append(`<option value="${object.keyword}">${object.keyword}</option>`);
+
+  }
 
 }
-render.prototype.dropDown = function(object){
-  let $list = $('option').clone();
 
-  $($list).append(`<option>${object.keyword}</option>`);
-  console.log('bro2');
-  $('select').append($list);
+
+function filterHorns(){
+  let $keyword = $(this).val();
+  // if the value of option picked === the cards class... show the matching cards and hide all others.
+  if($keyword){
+    $('#pictures').hide();
+    $(`.${$keyword}`).fadeIn();
+  }
+  else{
+    $('#pictures').show();
+  }
+  console.log('hey')
 }
-
-// imgTemp.prototype.dropDown = function {
-
-//     let $list = $('option').clone();
-//     let keyword = $(object).val();
-//     console.log('bro');
-//     $($list).text(object.keyword);
-//     console.log('bro2');
-//     $('select').append($list);
-// }
-
-// dropDown();
-
-
-
